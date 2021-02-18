9d442953d6e0c7691b70d8b6c5c6122dfd2c7077
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-02-18 04:22:51 -0300

    added new migrations/table for likes and followers and added some columns to users table
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -6,13 +6,9 @@ if (process.env.NODE_ENV === 'production') {
 	transporter = nodemailer.createTransport({
 		host: process.env.SMTP_HOST,
 		port: process.env.SMTP_PORT,
-		secure: true,
 		auth: {
 			user: process.env.SMTP_USER,
 			pass: process.env.SMTP_PASS,
-		},
-		tls: {
-			rejectUnauthorized: false,
 		},
 	});
 } else {
