799ac257f917fe56b4dbc7643c789daa7c6d6014
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-05-24 23:40:29 -0300

    fixed get notifications action
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/controllers/UserNotificationController.js
+++ src/controllers/UserNotificationController.js
@@ -9,14 +9,22 @@ module.exports = {
 			const { id } = req.token;
 			const daysAgo = new Date();
 			daysAgo.setDate(daysAgo.getDate() - 15);
-
 			const notifications = await knex('users_notifications')
-				.where({
-					to_user_id: id,
-					readed: false,
-				})
-				.andWhere('created_at', '>=', daysAgo)
-				.orderBy('created_at', 'desc');
+				.select(
+					"users_notifications.type", "users_notifications.social_media", "users_notifications.readed", "users_notifications.created_at",
+					knex.raw("json_object('username', users.username , 'social_profile_picture', users.social_profile_picture) as user"),
+					knex.raw("(select json_object('username', users.username , 'social_profile_picture', users.social_profile_picture) from users where users.id = "+ id +") as to_me")
+				)
+				.join('users', 'users_notifications.user_id', 'users.id')
+				.where('users_notifications.to_user_id', id)
+				.andWhere('users_notifications.created_at', '>=', daysAgo)
+				.orderBy('users_notifications.created_at', 'desc');
+			if (notifications.length > 0) {
+				notifications.map(notification  => {
+					notification.user = JSON.parse(notification.user);
+					notification.to_me = JSON.parse(notification.to_me);
+				});
+			}
 			return res.status(200).json(notifications);
 		} catch (error) {
 			next(error);
