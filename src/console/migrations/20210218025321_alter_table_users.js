00af159dba437e304dbfbdb9eb9173c06b04112b
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-09-15 11:37:53 -0300

    fix: changed migration error on unusable Address lib
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/console/migrations/20210218025321_alter_table_users.js
+++ src/console/migrations/20210218025321_alter_table_users.js
@@ -1,12 +1,10 @@
-const address = require('address');
-
 exports.up = (knex) => knex.schema.alterTable('users', (t) => {
 	t.integer('followers').defaultTo(0);
 	t.integer('likes').defaultTo(0);
 	t.integer('invited_friends').defaultTo(0);
 	t.integer('invited_total_points').defaultTo(0);
 	t.string('referral_code_applied', 30);
-	t.string('current_ip', 100).defaultTo(address.ip());
+	t.string('current_ip', 100).defaultTo(0);
 });
 
 exports.down = (knex) => knex.schema.dropTable('users');
