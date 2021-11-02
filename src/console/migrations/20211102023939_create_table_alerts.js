fb8623c2e427e45afb7bd865b7b96dcc45137237
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-11-02 16:34:58 -0300

    fix: fixed migration alerts table
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/console/migrations/20211102023939_create_table_alerts.js
+++ src/console/migrations/20211102023939_create_table_alerts.js
@@ -2,6 +2,7 @@ exports.up = (knex) => knex.schema.createTable('alerts', (t) => {
 	t.increments('id').primary();
 	t.string('title', 100);
     t.string('description', 999).notNull();
+    t.string('bg_color', 50).notNull();
     t.integer('mod_id').notNull().unsigned();
 
     t.foreign('mod_id').references('users.id');
