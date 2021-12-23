c213733b45754449866e8b4b6fd6520e6927f4a7
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-12-23 00:44:57 -0300

    feat: added get all user activities endpoint
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/controllers/UserNotificationController.js
+++ src/controllers/UserNotificationController.js
@@ -1,4 +1,7 @@
 const knex = require('../config/knex');
+const {
+    createPagination
+} = require('../utils');
 
 module.exports = {
 	async getUserNotifications(req, res, next) {
@@ -42,6 +45,34 @@ module.exports = {
 				.whereIn('id', notificationsList)
 				.update({ readed: true, updated_at: knex.fn.now() });
 			return res.json(notifications);
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	async getUserActivities(req, res, next) {
+		try {
+			const { id } = req.token;
+			const page = parseInt(req.query.page);
+            const limit = parseInt(req.query.limit);
+			if (!page || !limit) return next(new Error('Nenhuma pagina/limite definido'));
+			const allActivities = await knex('users_notifications');
+			const activities = await knex('users_notifications')
+				.select(
+					'users_notifications.id', 'users_notifications.type', 'users_notifications.social_media', 'users_notifications.readed', 'users_notifications.created_at',
+					knex.raw('json_object(\'username\', users.username , \'social_profile_picture\', users.social_profile_picture) as to_me'),
+					knex.raw('(select json_object(\'username\', users.username , \'social_profile_picture\', users.social_profile_picture) from users where users.id = '+ id +') as user')
+				)
+				.join('users', 'users_notifications.to_user_id', 'users.id')
+				.where({ 'users_notifications.user_id': id });
+
+			if (activities.length > 0) {
+				activities.map(activity  => {
+					activity.user = JSON.parse(activity.user);
+					activity.to_me = JSON.parse(activity.to_me);
+				});
+			}
+			return res.status(200).json({ activities, pagination: createPagination(allActivities.length, page, limit) });
 		} catch (error) {
 			next(error);
 		}
